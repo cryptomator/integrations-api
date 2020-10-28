ce5e186859c93aedd89f87bfe31b6ca1285dfa8d
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2020-10-28 15:14:47 +0100

    fixing javadoc

--- src/main/java/org/cryptomator/integrations/keychain/KeychainAccessIntegration.java
+++ src/main/java/org/cryptomator/integrations/keychain/KeychainAccessIntegration.java
@@ -10,12 +10,14 @@ public interface KeychainAccessIntegration {
 	 *
 	 * @param key        Key used to retrieve the passphrase via {@link #loadPassphrase(String)}.
 	 * @param passphrase The secret to store in this keychain.
+	 * @throws KeychainAccessException If storing the password failed
 	 */
 	void storePassphrase(String key, CharSequence passphrase) throws KeychainAccessException;
 
 	/**
 	 * @param key Unique key previously used while {@link #storePassphrase(String, CharSequence) storing a passphrase}.
 	 * @return The stored passphrase for the given key or <code>null</code> if no value for the given key could be found.
+	 * @throws KeychainAccessException If loading the password failed
 	 */
 	char[] loadPassphrase(String key) throws KeychainAccessException;
 
@@ -23,6 +25,7 @@ public interface KeychainAccessIntegration {
 	 * Deletes a passphrase with a given key.
 	 *
 	 * @param key Unique key previously used while {@link #storePassphrase(String, CharSequence) storing a passphrase}.
+	 * @throws KeychainAccessException If deleting the password failed
 	 */
 	void deletePassphrase(String key) throws KeychainAccessException;
 
@@ -31,12 +34,13 @@ public interface KeychainAccessIntegration {
 	 *
 	 * @param key        Unique key previously used while {@link #storePassphrase(String, CharSequence) storing a passphrase}.
 	 * @param passphrase The secret to be updated in this keychain.
+	 * @throws KeychainAccessException If changing the password failed
 	 */
 	void changePassphrase(String key, CharSequence passphrase) throws KeychainAccessException;
 
 	/**
-	 * @return <code>true</code> if this KeychainAccessStrategy works on the current machine.
-	 * @implNote This method must not throw any exceptions and should fail fast
+	 * @return <code>true</code> if this KeychainAccessIntegration works on the current machine.
+	 * @implSpec This method must not throw any exceptions and should fail fast
 	 * returning <code>false</code> if it can't determine availability of the checked strategy
 	 */
 	boolean isSupported();
