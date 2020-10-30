20d1c12cf4ec8029d5d59433609d10151c9a9657
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2020-10-30 09:23:23 +0100

    Added message-without-cause constructor

--- src/main/java/org/cryptomator/integrations/keychain/KeychainAccessException.java
+++ src/main/java/org/cryptomator/integrations/keychain/KeychainAccessException.java
@@ -2,7 +2,12 @@ package org.cryptomator.integrations.keychain;
 
 public class KeychainAccessException extends Exception {
 
+	public KeychainAccessException(String message) {
+		super(message);
+	}
+
 	public KeychainAccessException(String message, Throwable cause) {
 		super(message, cause);
 	}
+
 }
