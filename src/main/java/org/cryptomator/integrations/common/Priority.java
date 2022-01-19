9c6c76a8e93b96a01d456296b631704d16da4caf
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-01-19 15:39:44 +0100

    added new annotations for priorization and filtering

---
+++ src/main/java/org/cryptomator/integrations/common/Priority.java
@@ -0,0 +1,23 @@
+package org.cryptomator.integrations.common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Integration Priority.
+ * <p>
+ * If multiple implementations for an integration can be provided, the provider with the highest priority will be used.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Priority {
+	int DEFAULT = 0;
+	int FALLBACK = Integer.MIN_VALUE;
+
+	int value() default DEFAULT;
+}
