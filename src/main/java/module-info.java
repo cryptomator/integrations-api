fad104ccc52e4606002e21c005c3c29031b9f5d2
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-03-07 10:25:52 +0100

    export `org.cryptomator.integrations.common`

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module org.cryptomator.integrations.api {
 	requires static org.jetbrains.annotations;
 
 	exports org.cryptomator.integrations.autostart;
+	exports org.cryptomator.integrations.common;
 	exports org.cryptomator.integrations.keychain;
 	exports org.cryptomator.integrations.tray;
 	exports org.cryptomator.integrations.uiappearance;
