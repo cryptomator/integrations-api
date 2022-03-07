e4137ffb4d6506bcd1d968fe1b05962882e09a81
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-03-07 09:33:43 +0100

    api fine tuning and documentation

--- src/main/java/org/cryptomator/integrations/tray/TrayMenuController.java
+++ src/main/java/org/cryptomator/integrations/tray/TrayMenuController.java
@@ -7,6 +7,11 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Displays a tray icon and menu
+ *
+ * @since 1.1.0
+ */
 public interface TrayMenuController {
 
 	static Optional<TrayMenuController> get() {
@@ -14,19 +19,22 @@ public interface TrayMenuController {
 	}
 
 	/**
-	 * Adds an icon to the system tray.
+	 * Displays an icon on the system tray.
 	 *
-	 * @param iconData What image to show
-	 * @param tooltip  Text shown when hovering
-	 * @throws IOException thrown when interacting with the given <code>iconData</code>
+	 * @param rawImageData  What image to show
+	 * @param defaultAction Action to perform when interacting with the icon directly instead of its menu
+	 * @param tooltip       Text shown when hovering
+	 * @throws IOException thrown when interacting with the given <code>rawImageData</code>
 	 */
-	void showTrayIcon(InputStream iconData, String tooltip) throws IOException;
+	void showTrayIcon(InputStream rawImageData, Runnable defaultAction, String tooltip) throws IOException;
 
 	/**
 	 * Show the given options in the tray menu.
+	 * <p>
+	 * This method may be called multiple times, e.g. when the vault list changes.
 	 *
 	 * @param items Menu items
 	 */
-	void setTrayMenu(List<TrayMenuItem> items);
+	void updateTrayMenu(List<TrayMenuItem> items);
 
 }
