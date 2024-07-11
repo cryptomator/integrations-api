750bb022e381619496cac402072f94a50dec9dfa
Armin Schrenk <armin.schrenk@skymatic.de>
2024-07-11 16:09:37 +0200

    refactor to make localization an own annotation

--- src/main/java/org/cryptomator/integrations/common/LocalizedDisplayName.java
+++ src/main/java/org/cryptomator/integrations/common/LocalizedDisplayName.java
@@ -9,12 +9,26 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * A humanreadable name of the annotated class.
+ * A humanreadable, localized name of the annotated class.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @ApiStatus.Experimental
-public @interface DisplayName {
-	String value();
+public @interface LocalizedDisplayName {
+
+	/**
+	 * Name of the localization bundle, where the display name is loaded from.
+	 *
+	 * @return Name of the localization bundle
+	 */
+	String bundle();
+
+	/**
+	 * The localization key containing the display name.
+	 *
+	 * @return Localization key to use
+	 */
+	String key();
+
 }
--- src/main/java/org/cryptomator/integrations/common/NamedServiceProvider.java
+++ src/main/java/org/cryptomator/integrations/common/NamedServiceProvider.java
@@ -1,24 +1,32 @@
 package org.cryptomator.integrations.common;
 
+import java.util.ResourceBundle;
+
 /**
- * A service provider with a specific, human-readable name.
- * 
+ * A service provider with a human-readable, possibly localized name.
  */
 public interface NamedServiceProvider {
-    
-    /**
-     * Get the name of this service provider.
-     * @implNote The default implementation looks for the {@link DisplayName} annotation and uses its value. If the annotation is not present, it falls back to the qualified class name.
-     * @return The name of the service provider
-     * 
-     * @see DisplayName
-     */
-    default public String getName() {
-        var displayName = this.getClass().getAnnotation(DisplayName.class);
-        if(displayName != null) {
-            return displayName.value();
-        } else {
-            return this.getClass().getName();
-        }
-    }
+
+	/**
+	 * Get the name of this service provider.
+	 *
+	 * @return The name of the service provider
+	 * @implNote The default implementation looks first for a {@link LocalizedDisplayName} and loads the name from the specified resource bundle/key. If the annotation is not present, the code looks for {@link DisplayName} and uses its value. If none of the former annotations are present, it falls back to the qualified class name.
+	 * @see DisplayName
+	 * @see LocalizedDisplayName
+	 */
+	default String getName() {
+		var localizedDisplayName = this.getClass().getAnnotation(LocalizedDisplayName.class);
+		if (localizedDisplayName != null) {
+			return ResourceBundle.getBundle(localizedDisplayName.bundle()) //
+					.getString(localizedDisplayName.key());
+		}
+
+		var displayName = this.getClass().getAnnotation(DisplayName.class);
+		if (displayName != null) {
+			return displayName.value();
+		} else {
+			return this.getClass().getName();
+		}
+	}
 }
