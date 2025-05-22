94279178f750eb894d4610bdd21c2ad0252ca08c
Armin Schrenk <armin.schrenk@skymatic.de>
2025-05-22 11:57:34 +0200

    deprecate whole uiappearance package

---
+++ src/main/java/org/cryptomator/integrations/uiappearance/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Package for getting the OS color theme and listening to theme changes.
+ *
+ * @deprecated Cryptomator uses since version 1.14.0 the JavaFX framework in version 22, which provides via Platform.Preferences the system color scheme
+ */
+@Deprecated(since = "1.6.0")
+@ApiStatus.ScheduledForRemoval(inVersion = "1.7.0")
+package org.cryptomator.integrations.uiappearance;
+
+import org.jetbrains.annotations.ApiStatus;
