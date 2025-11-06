41d8e4cdefb300f892d450c4bf2d7c40495d5cb5
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2025-11-06 13:08:56 +0100

    delete unused class

--- src/main/java/org/cryptomator/integrations/update/BasicUpdateInfo.java
+++
@@ -1,4 +0,0 @@
-package org.cryptomator.integrations.update;
-
-public record BasicUpdateInfo(String version, UpdateMechanism<BasicUpdateInfo> updateMechanism) implements UpdateInfo<BasicUpdateInfo> {
-}
